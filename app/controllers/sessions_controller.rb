71c0f25222ad2bbc902465c87b8f8f2923fc6541
hovablack <nevermind92@hotmail.com>
2021-07-14 15:16:13 -0500

    Set styling and controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,44 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:session][:username].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid username/password'
+      render :new
+    end
+  end
+
+  def omniauth
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+      u.uid = auth['uid']
+      u.provider = auth['provider']
+      u.password = SecureRandom.hex(10)
+    end
+    if @user.valid?
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid credentials'
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,61 @@
 class UsersController < ApplicationController
+  before_action :login_required, only: [:index, :show, :edit, :update]
+  before_action :user_required, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    user_found
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params(:username, :email, :password))
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome Boozehound!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params(:username, :password))
+      flash[:success] = "Boozehound updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params(*args)
+    params.require(:user).permit(*args)
+  end
+
+  def user_found
+    @user = User.find(params[:id])
+  end
+
+  def login_required
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def user_required
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
