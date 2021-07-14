d6ba32a17337b8f15a69cd1ee7b1e2be6099d7bc
hovablack <nevermind92@hotmail.com>
2021-07-14 15:09:58 -0500

    Layouts and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
+  get 'auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get 'auth/failure', to: redirect('/')
+  get 'register', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :reviews
   resources :drinks
   resources :users
-  get 'sessions/new'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
