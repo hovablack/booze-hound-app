599dd9691ae00e938900617b187f729d3ab91f2e
hovablack <nevermind92@hotmail.com>
2021-07-14 15:12:55 -0500

    Set model associations/validations and helpers

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,3 +1,5 @@
 class Drink < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.username = username.downcase }
+  validates :username, presence: true, uniqueness: true,
+                       length: {maximum: 12}
+  validates :email, presence: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :drinks
+  has_many :reviews
+  has_many :reviewed_drinks, through: :reviews, source: :drink
 end
